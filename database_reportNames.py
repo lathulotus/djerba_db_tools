a75b3f73245ea876a8d9813ee86dae7787756a0f
OICR\lkalaranjan <lkalaranjan@oicr.on.ca>
2026-01-26 13:42:06 -0500

    Fixing ticket 1679

---
+++ database_reportNames.py
@@ -0,0 +1,8 @@
+# Ticket: 1679 (https://jira.oicr.on.ca/browse/GCGI-1679)
+# Archive different versions of reports on same requisition
+
+# Plan
+# 1. Identify report type under attributes
+# 2. Add attribute to naming convention
+# Ex: reportID-v1_research_report.json
+
