afbf9615ad115695c940dcbeae69215e3f87fe23
lathulotus <lathursharanjan@outlook.com>
2026-02-03 12:07:19 -0500

    Revised report naming for archive

---
+++ troubleshoot/database_reportNames/README.md
@@ -0,0 +1,6 @@
+## Archive different versions of reports on same requisition
+Ticket: [1679](https://jira.oicr.on.ca/browse/GCGI-1679)
+
+1) Identify report type under attributes
+2) Add attribute to naming convention
+- Ex: reportID-v1_report_research.json
--- troubleshoot/database_reportNames/database_reportNames.py
+++ troubleshoot/database_reportNames/database_reportNames.py
@@ -1,10 +1,3 @@
-# Ticket: 1679 (https://jira.oicr.on.ca/browse/GCGI-1679)
-# Archive different versions of reports on same requisition
-    # 1. Identify report type under attributes
-    # 2. Add attribute to naming convention
-    # Ex: reportID-v1_research_report.json
-
-
 """Interface with a CouchDB instance for JSON report documents"""
 
 
