905f6d3ae9621c6db9742ba42262038b160fe779
lathulotus <lathursharanjan@outlook.com>
2026-02-04 17:05:02 -0500

    Cleaned data retrieval script

--- 2_queryCouchDB/data_retrieval.py
+++ 2_queryCouchDB/data_retrieval.py
@@ -12,7 +12,7 @@ import os
 import json
 import logging
 import requests
-from typing import List, Dict, Any
+from typing import Any
 from djerba.core.base import base as core_base
 
 # Retrieve reports
@@ -21,22 +21,24 @@ class couchdb_reader(core_base):
 
     def __init__(self, log_level=logging.INFO, log_path=None):
         """Initialize CouchDB connection using .env variables."""
-        self.couch_url = (os.environ.get("COUCH_URL")).rstrip("/")
         self.couch_db = os.environ.get("COUCH_DB")
         self.couch_user = os.environ.get("COUCH_USER")
         self.couch_pass = os.environ.get("COUCH_PASS")
+        self.couch_url = os.environ.get("COUCH_URL")
+        if self.couch_url:
+            self.couch_url = self.couch_url.rstrip("/")
 
-        # warning for missing variables
-        missing = [name for name, value in{
-            "COUCH_URL": self.couch_url,
-            "COUCH_DB": self.couch_db,
-            "COUCH_USER": self.couch_user,
-            "COUCH_PASS": self.couch_pass
-        }.items() if value is None]
+        missing = [
+            name for name, value in{
+                "COUCH_URL": self.couch_url,
+                "COUCH_DB": self.couch_db,
+                "COUCH_USER": self.couch_user,
+                "COUCH_PASS": self.couch_pass
+            }.items() if value is None
+        ]
         if missing:
             raise RuntimeError(f"Missing required environment variable(s): {missing}")
         
-        # authorize log in
         self.auth = (self.couch_user, self.couch_pass)
         logger_name = "djerba:couchdb_reader"
         self.logger = self.get_logger(log_level, logger_name, log_path)
@@ -47,10 +49,7 @@ class couchdb_reader(core_base):
         url = f"{self.couch_url}/{self.couch_db}/{report_id}"
         self.logger.info(f"Fetching report by ID: {report_id}")
 
-        # get file from couchDB
         response = requests.get(url, auth=self.auth)
-        
-        # status of request
         response.raise_for_status()
 
         return response.json()
@@ -59,21 +58,18 @@ class couchdb_reader(core_base):
     def fetch_bulk(self, report_ids: list[str]) -> list[dict[str, Any]]:
         """Retrieve multiple reports based on a list of report IDs."""
         url = f"{self.couch_url}/{self.couch_db}/_bulk_get"
-        docIDs = {"docs": [{"id": repID for repID in report_ids}]}
+        docIDs = {"docs": [{"id": repID} for repID in report_ids]}
         self.logger.info(f"Fetching {len(report_ids)} reports.")
 
-        #get files from couchDB
         response = requests.post(
             url,
             auth=self.auth,
-            headers={"Content-Type": "applications/json"},
+            headers={"Content-Type": "application/json"},
             data=json.dumps(docIDs)
         )
 
-        # status of request
         response.raise_for_status()
 
-        # output of bulk fetching
         documents = []
         for result in response.json()["results"]:
             for doc in result["docs"]:
@@ -90,7 +86,6 @@ class couchdb_reader(core_base):
         querying ={"selector": selector}
         self.logger.info(f"Running metadata query: {selector}")
 
-        # get files from couchDB
         response = requests.post(
             url,
             auth=self.auth,
@@ -98,7 +93,6 @@ class couchdb_reader(core_base):
             data=json.dumps(querying)
         )
 
-        # status of request
         response.raise_for_status()
 
         return response.json().get("docs", [])
