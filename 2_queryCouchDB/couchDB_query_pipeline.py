8a4e02d0c2f1b58d2af9cfe4d4d91c2157de8108
lathulotus <lathursharanjan@outlook.com>
2026-02-24 11:57:59 -0500

    Wrapper to run query pipeline

--- 2_queryCouchDB/couchDB_query_pipeline.py
+++ 2_queryCouchDB/couchDB_query_pipeline.py
@@ -8,29 +8,6 @@ Usage:
     python3 couchDB_query_pipeline.py
 """
 
-import couchdb
-import json
-import os
-import yaml
-import argparse
-from datetime import datetime, timedelta
-import re
-import shutil
-import pandas as pd
-import matplotlib.pyplot as plt
-
-def read_login_file(file_path):
-    """ Reads input file and returns dict with couchDB login info """
-    params = {}
-    with open(file_path, "r") as f:
-        for line in f:
-            if ':' in line:
-                key, value = line.strip().split(':', 1)
-                params[key.strip()] = value.strip()
-    return params
-
-
-
 
 # subprocess.run(): combine arguments, parse, pass to both scripts
 
