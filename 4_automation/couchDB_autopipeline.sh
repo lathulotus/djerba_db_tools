4c949d0cb866e23f4815e9f7ba35b439d1760b21
lathulotus <lathursharanjan@outlook.com>
2026-03-25 16:59:27 -0400

    Fix paths in script for cron job

--- 4_automation/couchDB_autopipeline.sh
+++ 4_automation/couchDB_autopipeline.sh
@@ -3,10 +3,10 @@ cd ./queryTest
 
 # Run pipeline to generate master summary (CSV)
 python3 couchDB_query_pipeline.py --config couchDB_query_pipeline.yaml
-rm -rf filtered_jsons/
+rm -rf ./output/filtered_jsons/
 
 # Copy summary.csv to Marimo notebook
-cp output/summary.csv ./couchDB_marimo.py
+cp ./output/summary.csv ./couchDB_marimo.py
 
 # Runs weekly (Fri @ 6PM)
 # 0 18 * * 5 ./couchDB_autopipeline.sh
